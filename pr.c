43f90f936da6107e010e0545226cae6f3d810e7a
Duethy Elsa Mathews <77284995+Duethyem02@users.noreply.github.com>
2022-08-09 10:22:12 +0530

    Update pr.c

--- pr.c
+++ pr.c
@@ -1,106 +1,75 @@
-#include <stdio.h>
+#include<stdio.h>
 
-void swap (int *b, int *c)
+void swap(int *b,int *c)
 {
-
-int temp;
-temp = *c;
-
-*c = *b;
-
-*b= temp;
+	int temp=*c;
+	*c=*b;
+	*b=temp;
 
 }
 
 int main()
 {
-int i, j,n, cmpt;
-
-float twt=0,ttat=0;
-
-printf ("Enter number of processes: ");
-
-scanf("%d", &n);
-
-int a[n][6];
-
-printf("Enter arrival time and burst time of each prouss:\n");
-
-for(i=0;i<n; i++)
-
-{printf ("Paccess ID:"); 
-scanf("%d", &a[i][0]);
-printf ("A-T of process %d", a[i][0]); 
-scanf("%d", &a[i][1]); 
-printf("BT of process %d", a[i][0]);
-scanf("%d", &a[i][2]);
-printf ("pt of process %d", a[i][0]); 
-scanf("%d", &a[i][5]);
-} 
-for (i=0; i<n;i++)
-
-{
-
-for (j=0; j<n-i-1;j++)
-
-{ if (a[j][1]> a[j+1][1])
-
-{ swap (&a[j][0],& a[j+1][0]);
-
-swap (&a[j][1], &a[j+1][1]); 
-swap (& a[j][2], & a [j+1][2]);
-swap (& a[j][5], & a [j+1][5]);
-}
-
-}
-}
-a[0][3]= a[0][1]; //waiting time
-
-a[0][4]= a[0][2]- a[0][1]; // turnaround time
-
-cmpt = a[0][4]; // completion time
-
-twt=twt+a[0][3];
-
-ttat =ttat+ a[0][4];
-
-for(i=1; i<n;i++)
- {
-    int min = a[i][5];
-
-    for(j=i+1; j<n;j++)
- {
-
-if (min> a[j][5] && a[j][1] <= cmpt) {
-
-{
-
-min=a[j][5];
-
-swap (&a[i][0],&a[j][0]); 
-swap (&a[i][1],&a[j][1]);
-swap(&a[i][5],&a[j][5]);
-swap (& a[i][2],& a[j][2]);
-
-}
-}
-a[i][3]=cmpt-a[i][1];
-twt =twt+a[i][3];
-
-cmpt = cmpt +a[i][2];
-a[i][4]=cmpt-a[i][1];
-
-ttat =ttat+a[i][4];
-
-}
- printf ("Prouss\t A.T\t BT\tPR\tTAT\tWT\n"); 
-for (i=0;i<n; i++) {
-
-printf("%d\t%d\t%d\t%d\t%d\t%d\n", a[i][0], a[i][1], a[i][2],a[i][5], a[i][4], a[i][3]);
-
-} printf ("Average turnaround time = %f \n", ttat/n); 
-printf ("Average waiting time = %f \n", twt/n); 
-return 0;
-}
-
+	int i,j,n,cmpt;
+	float twt=0,ttat=0;
+	printf("Enter no.of processes:");
+	scanf("%d",&n);
+	int a[n][6];
+	printf("enter at and bt of each process:\n");
+	for(i=0;i<n;i++)
+	{
+		printf("ID:");
+		scanf("%d",&a[i][0]);
+		printf("A.T:");
+		scanf("%d",&a[i][1]);
+		printf("B.T:");
+		scanf("%d",&a[i][2]);
+		printf("priority:");
+		scanf("%d",&a[i][5]);
+	}
+	for(i=0;i<n;i++)
+	{
+		for(j=0;j<n-i-1;j++)
+		{
+			if(a[j][1]>a[j+1][1])
+			{
+				swap(&a[j][0],&a[j+1][0]);
+				swap(&a[j][1],&a[j+1][1]);
+				swap(&a[j][2],&a[j+1][2]);
+				swap(&a[j][5],&a[j+1][5]);
+			}
+		}
+	}
+	a[0][3]=a[0][1];
+	a[0][4]=a[0][2]-a[0][1];
+	cmpt=a[0][4];
+	twt+=a[0][3];
+	ttat+=a[0][4];
+	for(i=1;i<n;i++)
+	{
+		int min=a[i][5];
+		for(j=i+1;j<n;j++)
+		{
+			if(min>a[j][5] && a[j][1]<=cmpt )
+			{
+				min=a[j][5];
+				swap(&a[i][0],&a[j][0]);
+				swap(&a[i][1],&a[j][1]);
+				swap(&a[i][2],&a[j][2]);
+				swap(&a[i][5],&a[j][5]);
+			}
+		}
+		a[i][3]=cmpt-a[i][1];
+		twt+=a[i][3];
+		cmpt+=a[i][2];
+		a[i][4]=cmpt-a[i][1];
+		ttat+=a[i][4];
+	}
+	printf("Process\tA.T\tB.T\tP.R\tT.A.T\tW.T\n");
+	for(i=0;i<n;i++)
+	{
+		printf("%d\t%d\t%d\t%d\t%d\t%d\n",a[i][0],a[i][1],a[i][2],a[i][5],a[i][4],a[i][3]);
+	}
+	printf("Avg.TAT:%f\n",ttat/n);
+	printf("Avg.WT:%f\n",twt/n);
 }
